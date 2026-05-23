0c0d8ccc9d3b67fd4310ec618fc55d594eaece4c
UmeAiman <umeaimans.94@gmail.com>
2026-05-23 15:44:48 +0500

    Fix CORS for Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 
 const authRoutes = require('./routes/auth');
